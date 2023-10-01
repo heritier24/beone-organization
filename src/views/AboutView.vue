81ad5facc521884691ed0945476f9f0bfc330ebc
heritier24 <heritier.ganza@kudibooks.com>
2023-10-01 15:12:21 +0200

    about view improved to about component of admin

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<SideMenuAdmin />
+  <MainContentAbout />
 </template>
+<script>
+import SideMenuAdmin from '../components/SideMenuAdmin.vue'
+import MainContentAbout from '../components/MainContentAbout.vue'
+export default {
+  components: {
+    SideMenuAdmin,
+    MainContentAbout
+  }
+}
+</script>
