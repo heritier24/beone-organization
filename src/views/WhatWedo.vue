643cc68cfe53b75a78f481f06c67186d373f5500
heritier24 <heritier.ganza@kudibooks.com>
2023-10-01 15:17:17 +0200

    what we do service of admin to creating service from admin d

---
+++ src/views/WhatWedo.vue
@@ -0,0 +1,14 @@
+<template>
+<SideMenuAdmin />
+  <MainContentServive />
+</template>
+<script>
+import SideMenuAdmin from '../components/SideMenuAdmin.vue'
+import MainContentServive from '../components/MainContentServive.vue'
+export default {
+  components: {
+    SideMenuAdmin,
+    MainContentServive
+  }
+}
+</script>
