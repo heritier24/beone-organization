b5fd81579fe89ce00c0c1ba3ca7a44853113d307
heritier24 <heritier.ganza@kudibooks.com>
2023-10-01 15:16:23 +0200

    created home admin component

---
+++ src/views/HomeAdmin.vue
@@ -0,0 +1,14 @@
+<template>
+<SideMenuAdmin />
+  <MainContent />
+</template>
+<script>
+import SideMenuAdmin from '../components/SideMenuAdmin.vue'
+import MainContent from '../components/MainContent.vue'
+export default {
+  components: {
+    SideMenuAdmin,
+    MainContent
+  }
+}
+</script>
