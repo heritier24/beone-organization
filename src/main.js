656b508b09b5b7908f471ec64f7c12db3fe174ee
heritier24 <heritier.ganza@kudibooks.com>
2023-10-02 20:16:50 +0200

    imported axios to main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 createApp(App).use(store).use(router).mount('#app')
