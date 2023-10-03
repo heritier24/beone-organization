8d4eb1ccf8c5bf1fef2af4e590b35741886ff032
heritier24 <heritier.ganza@kudibooks.com>
2023-10-03 14:25:30 +0200

    storing title, description and service id of service selected

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    serviceId: '',
+    title: '',
+    description: ''
   },
   getters: {
   },
   mutations: {
+    setServiceId (state, serviceId) {
+      state.serviceId = serviceId
+    },
+    setTitle (state, title) {
+      state.title = title
+    },
+    setDescription (state, description) {
+      state.description = description
+    }
   },
   actions: {
   },
