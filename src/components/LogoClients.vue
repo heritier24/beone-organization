d79a3a2241619da1b59d5edd35c4c415e4e8bbaf
heritier24 <heritier.ganza@kudibooks.com>
2023-10-06 14:47:03 +0200

    client trusted us logo item menu created

---
+++ src/components/LogoClients.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="colImage">
+    <img :src="logoImage" class="img-fluid" alt="" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    colImage: {
+      type: String,
+      default: 'col-sm-2'
+    },
+    logoImage: {
+      type: String,
+      default: 'assets/img/clients/client-1.png'
+    }
+  }
+}
+</script>
