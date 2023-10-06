06680dedec2be984c2ad190edf3469566d0a6a44
heritier24 <heritier.ganza@kudibooks.com>
2023-10-06 14:46:23 +0200

    trusted us sub menu

---
+++ src/views/ClientsTrusted.vue
@@ -0,0 +1,14 @@
+<template>
+<SideMenuAdmin />
+  <MainContentClientsTrusted />
+</template>
+<script>
+import SideMenuAdmin from '../components/SideMenuAdmin.vue'
+import MainContentClientsTrusted from '../components/MainContentClientsTrusted.vue'
+export default {
+  components: {
+    SideMenuAdmin,
+    MainContentClientsTrusted
+  }
+}
+</script>
