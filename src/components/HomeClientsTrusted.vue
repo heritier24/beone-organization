a60cdc51d68d97094c8482f0225bd37aec41ce81
heritier24 <heritier.ganza@kudibooks.com>
2023-10-08 16:07:15 +0200

    client home trusted us improved well

---
+++ src/components/HomeClientsTrusted.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-sm-2">
+    <img :src="logoImage" class="img-fluid" alt="" /><span
+      hidden
+      >{{ logoID }}</span
+    >
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    logoID: {
+      type: String,
+      default: ''
+    },
+    colImage: {
+      type: String,
+      default: 'col-sm-2'
+    },
+    logoImage: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
