a98105c39f59b39426ae8ba0f3abeaecfe18705b
heritier24 <heritier.ganza@kudibooks.com>
2023-10-08 16:15:03 +0200

    testimonial admin component created well

---
+++ src/views/TestimonialsAdmin.vue
@@ -0,0 +1,14 @@
+<template>
+<SideMenuAdmin />
+  <MainContentTestimonials />
+</template>
+<script>
+import SideMenuAdmin from '../components/SideMenuAdmin.vue'
+import MainContentTestimonials from '../components/MainContentTestimonials.vue'
+export default {
+  components: {
+    SideMenuAdmin,
+    MainContentTestimonials
+  }
+}
+</script>
